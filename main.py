b2255c3426eec7f2e194b674b55a3a83859523b7
Angel390 <angel.arias48@gmail.com>
2023-06-26 14:00:33 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ import imageio
 import matplotlib
 import os
 import tkinter
+
+def cartoonify(image_path):
+    original_image = cv2.imread(image_path)
+    original_imag = cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB)
+
+# upload image from local system
+def upload():
+    image_path = easygui.fileopenbox()
+    #cartoonify(image_path)
